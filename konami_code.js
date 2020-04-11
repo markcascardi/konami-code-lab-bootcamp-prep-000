8721d57c4a8e87b2508ed2b620fa8ed7cfdab258
Mark Cascardi <mcascardi@gmail.com>
2020-04-11 17:09:56 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,16 +17,16 @@ function init() {
   document.body.addEventListener("keydown", (event) => {
     const key = event.key;
 
-    // if (key === codes[index]) {
-    //   index++;
-    //
-    //   if (index === codes.length) {
-    //     alert("Hurray!");
-    //
-    //     index = 0;
-    //   }
-    // } else {
-    //   index = 0;
-    // }
+    if (key === codes[index]) {
+      index++;
+    
+      if (index === codes.length) {
+        alert("Hurray!");
+    
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
   })
 }
